7af803e932150eb866fa10b4d39ee0a8169de154
Simon Su <simonsu.mail@gmail.com>
2018-11-17 00:20:45 +0800

    sync

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.all('/test', function(req, res, next){
+    res.send({
+        url: req.url,
+		    remote_url: req.connection.remoteAddress,
+        method: req.method,
+		    header: req.headers,
+        body: req.body,
+        q: req.query
+    })
+});
+
 module.exports = router;
